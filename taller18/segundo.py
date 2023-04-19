bc4f269ac66983203f6078cc2a1e46941e6af825
DanielBautistaMr <111470267+DanielBautistaMr@users.noreply.github.com>
2023-04-19 07:53:07 -0500

    commit 18 CORREGIDOOO

--- taller18/segundo.py
+++ taller18/segundo.py
@@ -1,26 +1,34 @@
 import numpy as np
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-x = np.array([0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5])
-y = np.array([4.2,6.5,7.5,8,8.5,8.8,9,9.1])
+xn=np.array([0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5])
 
+yn=np.array([4.2, 6.5, 7.5, 8, 8.5, 8.8, 9, 9.1])
 
-def exp_func(x, a, b):
-    return a * np.exp(b * x)
+n=len(xn)
 
-params, cov = curve_fit(exp_func, x, y)
+Sum_x=sum(xn)
+Sum_y=sum(yn)
+Sum_xx=sum(xn**2)
+Sum_xy=sum(xn*yn)
 
+print(Sum_x, Sum_y, Sum_xx, Sum_xy)
 
-y_est = exp_func(x, params[0], params[1])
+a_0=(Sum_xx*Sum_y-Sum_xy*Sum_x)/(n*Sum_xx-Sum_x**2)
+a_1=(n*Sum_xy-Sum_x*Sum_y)/(n*Sum_xx-Sum_x**2)
+print(a_0, a_1)
 
-a, b = params
+x=np.linspace(0, 9)
+y=a_0+a_1*x
 
-plt.scatter(x, y)
-plt.plot(x, y_est, 'r-')
-plt.title('Regresión exponencial')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.figure(1)
+
+plt.scatter(xn, yn, color='b')
+plt.grid(linestyle='dotted')
+plt.plot(x, y, color='r')
+
+plt.title(r'Regresión por Mínimos', fontsize=10)
+plt.xlabel(r'x', fontsize=10)
+plt.ylabel(r'y', fontsize=10)
+
 plt.show()
-
-print(f"La función exponencial ajustada es: y = {a:.2f} * e^({b:.2f}x)")
