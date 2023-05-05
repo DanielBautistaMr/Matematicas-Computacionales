97dc4cd1efdd97c022291993e72978a243a28eb4
DanielBautistaMr <111470267+DanielBautistaMr@users.noreply.github.com>
2023-05-05 07:23:27 -0500

    Taller 23

---
+++ Taller23/main.py
@@ -0,0 +1,53 @@
+
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+
+
+xi = np.array([0, 1, 2, 3, 4])
+fi = np.array([1, 0.9, -1, -2.3, 1.8])
+
+
+n = len(xi)
+x = sym.Symbol('x')
+polinomio = 0
+divisorL = np.zeros(n, dtype = float)
+for i in range(0,n,1):
+
+    numerador = 1
+    denominador = 1
+    for j  in range(0,n,1):
+        if (j!=i):
+            numerador = numerador*(x-xi[j])
+            denominador = denominador*(xi[i]-xi[j])
+    terminoLi = numerador/denominador
+
+    polinomio = polinomio + terminoLi*fi[i]
+    divisorL[i] = denominador
+
+
+polisimple = polinomio.expand()
+
+
+px = sym.lambdify(x,polisimple)
+
+
+muestras = 101
+a = np.min(xi)
+b = np.max(xi)
+pxi = np.linspace(a,b,muestras)
+pfi = px(pxi)
+
+
+print('    valores de fi: ',fi)
+print('divisores en L(i): ',divisorL)
+print()
+print('Polinomio de Lagrange: ')
+print(polisimple)
+
+
+plt.plot(xi,fi,'o', label = 'Puntos')
+plt.plot(pxi,pfi, label = 'Polinomio')
+plt.legend()
+plt.title('Interpolación Lagrange')
+plt.show()
